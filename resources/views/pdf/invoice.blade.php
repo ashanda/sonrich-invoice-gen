1b16b422f65df5ad3bb3ee4a9e840a0f57059618
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-06-15 17:25:48 +0530

    report

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
     <title>Invoice</title>
 </head>
 <style>
